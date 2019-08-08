4282022879acd76dbb8782c4b4276b8b55c1c6f6
Evan Wegley <tomocafe@pm.me>
2019-08-08 11:38:26 -0700

    Initial structure, using reveal.js 3.8.0 as submodule; setup teals directory for customization, template.html

---
+++ README.md
@@ -0,0 +1,6 @@
+# reveal-teals
+
+A template and plugin set for [`reveal-js`](https://github.com/hakimel/reveal.js) designed for use in the [TEALS](https://tealsk12.org) program. This framework can be generally used for classroom teaching.
+
+This framework is not officially supported or endorsed by TEALS. As of now, it is a work in progress!
+
---
+++ teals/js/teals.js
@@ -0,0 +1,11 @@
+// More info about config & dependencies:
+// - https://github.com/hakimel/reveal.js#configuration
+// - https://github.com/hakimel/reveal.js#dependencies
+Reveal.initialize({
+	dependencies: [
+		{ src: 'revealjs/plugin/markdown/marked.js' },
+		{ src: 'revealjs/plugin/markdown/markdown.js' },
+		{ src: 'revealjs/plugin/notes/notes.js', async: true },
+		{ src: 'revealjs/plugin/highlight/highlight.js', async: true }
+	]
+});
