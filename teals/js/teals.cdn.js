14d47c077fedecbf90ff03d1f0f9cc311bed2ae2
tomocafe <tomocafe@pm.me>
2019-09-05 19:25:46 -0700

    Separate templates for offline and standalone modes

---
+++ teals/js/teals.cdn.js
@@ -0,0 +1,11 @@
+// More info about config & dependencies:
+// - https://github.com/hakimel/reveal.js#configuration
+// - https://github.com/hakimel/reveal.js#dependencies
+Reveal.initialize({
+	dependencies: [
+		{ src: 'https://tomocafe.github.io/reveal-teals/revealjs/plugin/markdown/marked.js' },
+		{ src: 'https://tomocafe.github.io/reveal-teals/revealjs/plugin/markdown/markdown.js' },
+		{ src: 'https://tomocafe.github.io/reveal-teals/revealjs/plugin/notes/notes.js', async: true },
+		{ src: 'https://tomocafe.github.io/reveal-teals/revealjs/plugin/highlight/highlight.js', async: true }
+	]
+});
